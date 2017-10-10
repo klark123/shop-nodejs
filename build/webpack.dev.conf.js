65320a6838b869c0e0314c7b3b6982352bab6212
Egor Piskunou <egor.piskunou@gmail.com>
2017-10-10 18:40:25 +0300

    Initial commit

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,27 @@
+const utils = require('./utils');
+const webpack = require('webpack');
+const config = require('./config');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'src/index.html',
+      template: 'src/index.html',
+      inject: true
+    }),
+    new FriendlyErrorsPlugin()
+  ]
+});
